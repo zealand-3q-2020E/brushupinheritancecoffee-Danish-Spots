6ddb17806d1d584f9f6ef7ddeaa2cc74cec8028a
Justin Macdonald Hansen <just1392@edu.easj.dk>
2020-09-01 18:27:18 +0200

    Created the abstract method for strength and implemented it

--- CoffeeShopConsoleApp/BlackCoffee.cs
+++ CoffeeShopConsoleApp/BlackCoffee.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 
 namespace CoffeeShopConsoleApp
 {
     class BlackCoffee : Coffee
     {
+        public override string Strength()
+        {
+            return "Strong";
+        }
     }
 }
