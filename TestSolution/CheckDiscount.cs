06db86f761683756bc9a9760ce0afc4f8021e32f
Justin Macdonald Hansen <just1392@edu.easj.dk>
2020-09-02 20:47:31 +0200

    Created a unit test class that checks for if the discount on individual price methods throws an exception when discount is greater than 5

---
+++ TestSolution/CheckDiscount.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CoffeeShopConsoleApp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestSolution
+{
+    [TestClass]
+    public class CheckDiscount
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CheckExceptionBlackCoffee()
+        {
+            //Arrange
+            int discount = 6;
+
+            //Act
+            Coffee bc = new BlackCoffee(discount);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CheckExceptionCortado()
+        {
+            //Arrange
+            int discount = 6;
+
+            //Act
+            Coffee c = new Cortado(discount);
+        }
+
+        [TestMethod]
+        public void CheckExceptionEspresso()
+        {
+            //Arrange
+            int discount = 6;
+
+            //Act
+            Coffee e = new Espresso("sdag");
+
+            //Assert
+            Assert.AreEqual(20, e.price());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CheckExceptionFlatWhite()
+        {
+            //Arrange
+            int discount = 6;
+
+            //Act
+            Coffee fw = new FlatWhite(discount);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CheckExceptionLatte()
+        {
+            //Arrange
+            int discount = 6;
+
+            //Act
+            Coffee l = new Latte(discount);
+        }  
+    }
+}
